ae9c9d335aba1fdea73724b7bd173cc6738eaa87
Pierce <pierce@Pierces-MacBook-Pro-3.local>
2020-10-09 18:14:13 +0100

    Contact components added

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,75 @@
+import React, { useReducer } from 'react';
+import uuod from 'uuid';
+import ContactContext from './ContactContext';
+import ContactReducer from './ContactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+import contactContext from './ContactContext';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'Harry White',
+        email: 'harry@gmail.com',
+        phone: '333-333-3333',
+        type: 'professional',
+      },
+    ],
+  };
+
+  // From Reducer hook
+  // state allows us to access anything in our state
+  // Dispatch allows us to dispatch objects to the reducer
+  const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    // Return provider so we can wrap the app with this here context
+    <ContactContext.Provider
+      value={{
+        // Access to state and actions for components
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
